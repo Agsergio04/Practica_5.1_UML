a50a2d35a95b6bd853a42814fddb1cddc00410bf
Agsergio04 <agsergio@iesrafaelalberti.es>
2025-01-22 16:46:06 +0100

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
-    val cursoKotlin = Curso("Programación", "KOT101",)
+    val curso = Curso("Programación", "KOT101",)
     val estudiante1 = Estudiante("Paula Tocino", "12345678A")
     val estudiante2 = Estudiante("Coral Ruíz", "87654321B")
     val estudiante3 = Estudiante("Agarrido Rey", "87654321B")
 
-    estudiante1.inscribirse(cursoKotlin)
-    estudiante2.inscribirse(cursoKotlin)
-    estudiante3.inscribirse(cursoKotlin)
+    estudiante1.inscribirse(curso)
+    estudiante2.inscribirse(curso)
+    estudiante3.inscribirse(curso)
 
     cursoKotlin.mostrarEstudiantes()
 }
